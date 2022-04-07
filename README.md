d8bd85d5f93554706ddf48136861c0635ffc66d2
RedThread <670504507@qq.com>
2022-04-07 20:23:39 +0800

    case1-ffi

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ learn rust notes
 > caculate hash value of a file.  
 > what i learned:  
 > #1 how to enable crate feature -> alloc
+
+- case1-FFI
+> try to call C function api that include in libc from rust block
+> i choosed abs api for test
+> search the function signature of abs from <https://en.cppreference.com/w/c/numeric/math/abs>
+> "int        abs( int n );"
+> so give it a try.
---
+++ case1-ffi/src/main.rs
@@ -0,0 +1,20 @@
+// this extern block links to the libm library
+#[link(name = "c")]
+extern {
+    // this is a foreign function
+    fn abs(z: i32) -> i32;
+}
+
+// Since calling foreign functions is considered unsafe,
+// it's common to write safe wrappers around them.
+fn abs_ffi(z: i32) -> i32 {
+    unsafe { abs(z) }
+}
+
+fn main() {
+    let z = -123;
+
+    println!("abs({:?}) = {:?}", z, abs_ffi(z));
+}
+
+//abs(-123) = 123
